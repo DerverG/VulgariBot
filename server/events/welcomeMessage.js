35994366ff61a4a8202ed650da871d893a9c1c74
Derver <93274217+DerverG@users.noreply.github.com>
2025-04-16 01:55:57 -0700

    feat: enhance welcome message handling with dynamic embed support

--- server/events/welcomeMessage.js
+++ server/events/welcomeMessage.js
@@ -21,20 +21,46 @@ module.exports = (client) => {
             }
 
             // Datos del mensaje
-            // message = await fetchWelcomeMessage(member.guild.id)
-            
+            // const welcomeMessage = await fetchWelcomeMessage(member.guild.id)
+            const welcomeMessage = {
+                message: 'Prueba',
+                messageEmbed: {
+                    enabled: true,
+                    title: 'Title',
+                    description: 'Description',
+                    color:'#58b9ff',
+                    imageURL: 'https://th.bing.com/th/id/OIP.SwnZ7qbGY63-H6uKAAA3SgHaDt?rs=1&pid=ImgDetMain',
+                    thumbnailURL: 'ttps://i0.wp.com/brunchvirals.com/wp-content/uploads/2021/03/Image-Of-Examples-Of-Cute-Pfp-For-TikTok.png?w=1300&ssl=1',
+                    footerText: 'Footer'
+                }
+            }
 
-            const embed = new EmbedBuilder()
-                .setTitle('Title')
-                .setDescription('Description')
-                .setColor('#58b9ff')
-                .setImage('https://th.bing.com/th/id/OIP.SwnZ7qbGY63-H6uKAAA3SgHaDt?rs=1&pid=ImgDetMain')
-                .setThumbnail('https://i0.wp.com/brunchvirals.com/wp-content/uploads/2021/03/Image-Of-Examples-Of-Cute-Pfp-For-TikTok.png?w=1300&ssl=1')
-                .setFooter('Footer')
+            let embed = null
+
+            if (welcomeMessage.messageEmbed.enabled) {
+                embed = new EmbedBuilder()
+                    .setTitle(welcomeMessage.messageEmbed.title)
+                    .setDescription(welcomeMessage.messageEmbed.description)
+                    .setColor(welcomeMessage.messageEmbed.color)
+                    .setImage(welcomeMessage.messageEmbed.imageURL)
+                    .setThumbnail(welcomeMessage.messageEmbed.thumbnailURL)
+                    .setFooter(welcomeMessage.messageEmbed.footerText)
+            }
 
 
             // Enviar mensaje de bienvenida
-            channel.send({ embeds: [embed] })
+            if ( welcomeMessage.message && embed ) {
+                channel.send({
+                    content: welcomeMessage.message,
+                    embeds: [embed],
+                })
+            } else if (welcomeMessage.message) {
+                channel.send(welcomeMessage.message)
+            } else if (embed) {
+                channel.send({
+                    embeds: [embed],
+                })
+            }
         } catch (err) {
             console.error('Error handling GuildMemberAdd event:', err)
         }
