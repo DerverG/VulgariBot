d25a03af563bf821eedda595cfc5ded8c2696e51
Derver <93274217+DerverG@users.noreply.github.com>
2025-04-15 18:50:36 -0700

    fix: update prefix command to use correct serverPrefix from config

--- server/commands/prefix/prefix.js
+++ server/commands/prefix/prefix.js
@@ -8,7 +8,7 @@ module.exports = {
     run: async(client, message, args, config) => {
         if (!args[0]) return message.reply('Debes especificar un nuevo prefijo.')
         if (args[0].length > 3) return message.reply('El prefijo no puede tener más de 3 caracteres.')
-        if (args[0] === config.prefix) return message.reply('Ese es el prefijo actual.')
+        if (args[0] === config.serverPrefix) return message.reply('Ese es el prefijo actual.')
             
         await axiosClient.patch(`/server/updateServerConfig`, {
             serverGuildId: message.guild.id,
